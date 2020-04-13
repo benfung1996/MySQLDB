8ba08766122fe221ed269218a34d5e7216713559
Ben Fung <benfung1996@gmail.com>
2020-04-12 17:06:52 -0700

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,8 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var fs = require("fs");
 
+var cart = [];
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -16,10 +18,98 @@ connection.connect(function (err) {
 });
 
 function start() {
-    connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
         if (err) throw err;
 
         console.log(res);
-        connection.end();
+        purchase();
+    });
+};
+
+function purchase() {
+    inquirer.prompt({
+        name: "buy",
+        type: "list",
+        message: "Which product would you like to purchase?",
+        choices: ["Pen", "Eraser", "Paper", "Bottle Water", "Coffee", "Coke", "Banana", "Apple", "Avocado", "Sandwich", "Exit"]
     })
-}
+        .then(function (answer) {
+            switch (answer.buy) {
+
+                case "Pen":
+                    cart.push("Pen");
+                    amount();
+                    break;
+
+                case "Eraser":
+                    cart.push("Eraser");
+                    amount();
+                    break;
+
+                case "Paper":
+                    cart.push("Paper");
+                    amount();
+                    break;
+
+                case "Bottle Water":
+                    cart.push("Bottle Water");
+                    amount();
+                    break;
+
+                case "Coffee":
+                    cart.push("Coffee");
+                    amount();
+                    break;
+
+                case "Coke":
+                    cart.push("Coke");
+                    amount();
+                    break;
+
+                case "Banana":
+                    cart.push("Banana");
+                    amount();
+                    break;
+
+                case "Apple":
+                    cart.push("Apple");
+                    amount();
+                    break;
+
+                case "Avocado":
+                    cart.push("Avocado");
+                    amount();
+                    break;
+
+                case "Sandwich":
+                    cart.push("Sandwich");
+                    amount();
+                    break;
+
+                case "Exit":
+                    console.log("See you next time!")
+                    connection.end();
+                    break;
+            };
+        });
+};
+
+function amount() {
+    inquirer.prompt({
+        name: "amount",
+        type: "input",
+        message: "How many would you like?",
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            console.log("Please enter a number.")
+            return false;
+        }
+    })
+        .then(function (answer) {
+            console.log("Checking for product availability. Please wait.");
+
+        });
+};
+
